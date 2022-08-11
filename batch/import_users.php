35463689808b7f6d898d5530ea4227200bc4c89a
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2022-08-11 19:38:58 +0900

    社員情報登録バッチ実装２

---
+++ batch/import_users.php
@@ -0,0 +1,98 @@
+<?php
+
+//データベース接続
+$DB_USER = 'docker_php_batch_user';
+$DB_PASSWORD = 'docker_php_batch_pass';
+$DB_HOST = 'db';
+$DB = "docker_php_batch_db";
+$pdo = new PDO("mysql:host={$DB_HOST};dbname={$DB};charset=utf8", $DB_USER, $DB_PASSWORD);
+
+//社員情報CSVオープン
+$fp = fopen(__DIR__ . "/import_users.csv", "r");
+
+//トランザクション開始
+$pdo->beginTransaction();
+
+while ($data = fgetcsv($fp)) {
+    // var_dump($data);
+    //社員番号をキーに社員情報取得SQLの実行
+    $sql = "SELECT COUNT(*) AS count FROM users WHERE id = :id";
+    $param = [":id" => $data[0]];
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($param);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    //debug
+    // var_dump($data[0]);
+    // var_dump($result);
+
+    //SQLの検索結果は0件？
+    if ($result['count'] === "0") {
+        //社員情報SQLの実行
+        // var_dump($data[0]);
+        // var_dump('登録');
+        $sql = "INSERT INTO users ( ";
+        $sql .=" id, ";
+        $sql .=" name, ";
+        $sql .=" name_kana, ";
+        $sql .=" birthday, ";
+        $sql .=" gender, ";
+        $sql .=" organization, ";
+        $sql .=" post, ";
+        $sql .=" start_date, ";
+        $sql .=" tel, " ;
+        $sql .=" mail_address, ";
+        $sql .=" created, ";
+        $sql .=" updated ";
+        $sql .=") VALUES (";
+        $sql .=" :id, ";
+        $sql .=" :name, ";
+        $sql .=" :name_kana, ";
+        $sql .=" :birthday, ";
+        $sql .=" :gender, ";
+        $sql .=" :organization, ";
+        $sql .=" :post, ";
+        $sql .=" :start_date, ";
+        $sql .=" :tel, " ;
+        $sql .=" :mail_address, ";
+        $sql .=" NOW(), "; //作成日時
+        $sql .=" NOW() ";  //更新日時
+        $sql .=")";
+    } else {
+        //社員情報SQLの実行
+        // var_dump($data[0]);
+        // var_dump('更新');
+        $sql = "UPDATE users ";
+        $sql .="SET name = :name, ";
+        $sql .=" name_kana = :name_kana, ";
+        $sql .=" birthday = :birthday, ";
+        $sql .=" gender = :gender, ";
+        $sql .=" organization = :organization, ";
+        $sql .=" post = :post, ";
+        $sql .=" start_date = :start_date, ";
+        $sql .=" tel = :tel, ";
+        $sql .=" mail_address = :mail_address, ";
+        $sql .=" updated = NOW() "; //更新日時
+        $sql .="WHERE id = :id ";
+    }
+    $param = array(
+        "id" => $data[0],
+        "name" => $data[1],
+        "name_kana" => $data[2],
+        "birthday" => $data[3],
+        "gender" => $data[4],
+        "organization" => $data[5],
+        "post" => $data[6],
+        "start_date" => $data[7],
+        "tel" => $data[8],
+        "mail_address" => $data[9],
+    );
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($param);
+}
+
+//コミット
+$pdo->commit();
+
+//社員情報CSVクローズ
+fclose($fp);
